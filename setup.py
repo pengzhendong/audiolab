4ce31f1edf9913987afd3621dc1adff2924df1a2
pengzhendong <275331498@qq.com>
2025-04-14 12:08:52 +0800

    simplify setup.py

--- setup.py
+++ setup.py
@@ -28,9 +28,6 @@ setup(
     url="https://github.com/pengzhendong/audiolab",
     packages=find_packages(),
     include_package_data=True,
-    package_data={
-        "audiolab": ["reader/templates/*.txt"],
-    },
     install_requires=requirements,
     entry_points={
         "console_scripts": [
